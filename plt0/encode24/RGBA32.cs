181aa173b8077a898aa6204fa14092d06b01ef64
yoshakami <yohgui29@gmail.com>
2024-07-01 21:52:07 +0200

    Working RGBA32 (only cmpr 24 bits left)

--- plt0/encode24/RGBA32.cs
+++ plt0/encode24/RGBA32.cs
@@ -13,7 +13,7 @@ class RGBA32_class24
     public void RGBA32(List<byte[]> index_list, byte[] bmp_image, byte[] index)
     {
         int j = 0;
-        int bitmap_width4 = _plt0.bitmap_width << 3;
+        int diff = (_plt0.canvas_width - _plt0.bitmap_width) * 3;
         /* 4x4 pixel block
          * warning: THESE ARE BYTES
          * I4M SERIOUS ALL OTHERS ABOVE ARE BITS BUT THIS ONE IS BYTES
@@ -37,19 +37,19 @@ class RGBA32_class24
                         index[j + 1] = (byte)(bmp_image[i + _plt0.rgba_channel[0]] * _plt0.custom_rgba[0]);   // R
                         index[j + 8] = (byte)(bmp_image[i + _plt0.rgba_channel[1]] * _plt0.custom_rgba[1]);   // G
                         index[j + 9] = (byte)(bmp_image[i + _plt0.rgba_channel[2]] * _plt0.custom_rgba[2]);       // B
-                        if (j + 4 >= bitmap_width4)
+                        if (i + 5 < _plt0.bmp_filesize)
                         {
                             index[j + 2] = (byte)(255 * _plt0.custom_rgba[3]);   // A
                             index[j + 3] = (byte)(bmp_image[i + 3 + _plt0.rgba_channel[0]] * _plt0.custom_rgba[0]);   // R
                             index[j + 10] = (byte)(bmp_image[i + 3 + _plt0.rgba_channel[1]] * _plt0.custom_rgba[1]);  // G
                             index[j + 11] = (byte)(bmp_image[i + 3 + _plt0.rgba_channel[2]] * _plt0.custom_rgba[2]);  // B
-                            if (j + 8 >= bitmap_width4)
+                            if (i + 8 < _plt0.bmp_filesize)
                             {
                                 index[j + 4] = (byte)(255 * _plt0.custom_rgba[3]);  // A
                                 index[j + 5] = (byte)(bmp_image[i + 6 + _plt0.rgba_channel[0]] * _plt0.custom_rgba[0]);  // R
                                 index[j + 12] = (byte)(bmp_image[i + 6 + _plt0.rgba_channel[1]] * _plt0.custom_rgba[1]);  // G
                                 index[j + 13] = (byte)(bmp_image[i + 6 + _plt0.rgba_channel[2]] * _plt0.custom_rgba[2]);  // B
-                                if (j + 12 >= bitmap_width4)
+                                if (i + 11 < _plt0.bmp_filesize)
                                 {
                                     index[j + 6] = (byte)(255 * _plt0.custom_rgba[3]);  // A
                                     index[j + 7] = (byte)(bmp_image[i + 9 + _plt0.rgba_channel[0]] * _plt0.custom_rgba[0]);  // R
@@ -59,10 +59,11 @@ class RGBA32_class24
                             }
                         }
                         j += 16;
-                        if (j == bitmap_width4)
+                        if (j == index.Length)
                         {
                             j = 0;
-                            i += _plt0.bitmap_width % 4;  // 24 edit
+                            i += _plt0.bitmap_width % 4;
+                            i -= diff;
                             index_list.Add(index.ToArray());
                         }
 
@@ -78,19 +79,19 @@ class RGBA32_class24
                         index[j + 1] = (byte)(bmp_image[i + _plt0.rgba_channel[0]]);   // R
                         index[j + 8] = (byte)(bmp_image[i + _plt0.rgba_channel[1]]);   // G
                         index[j + 9] = (byte)(bmp_image[i + _plt0.rgba_channel[2]]);       // B
-                        if (j + 4 >= bitmap_width4)
+                        if (i + 5 < _plt0.bmp_filesize)
                         {
                             index[j + 2] = (byte)(255);   // A
                             index[j + 3] = (byte)(bmp_image[i + 3 + _plt0.rgba_channel[0]]);   // R
                             index[j + 10] = (byte)(bmp_image[i + 3 + _plt0.rgba_channel[1]]);  // G
                             index[j + 11] = (byte)(bmp_image[i + 3 + _plt0.rgba_channel[2]]);  // B
-                            if (j + 8 >= bitmap_width4)
+                            if (i + 8 < _plt0.bmp_filesize)
                             {
                                 index[j + 4] = (byte)(255);  // A
                                 index[j + 5] = (byte)(bmp_image[i + 6 + _plt0.rgba_channel[0]]);  // R
                                 index[j + 12] = (byte)(bmp_image[i + 6 + _plt0.rgba_channel[1]]);  // G
                                 index[j + 13] = (byte)(bmp_image[i + 6 + _plt0.rgba_channel[2]]);  // B
-                                if (j + 12 >= bitmap_width4)
+                                if (i + 11 < _plt0.bmp_filesize)
                                 {
                                     index[j + 6] = (byte)(255);  // A
                                     index[j + 7] = (byte)(bmp_image[i + 9 + _plt0.rgba_channel[0]]);  // R
@@ -100,10 +101,11 @@ class RGBA32_class24
                             }
                         }
                         j += 16;
-                        if (j == bitmap_width4)
+                        if (j == index.Length)
                         {
                             j = 0;
-                            i += _plt0.bitmap_width % 4;  // 24 edit
+                            i += _plt0.bitmap_width % 4;
+                            i -= diff;
                             index_list.Add(index.ToArray());
                         }
                     }
